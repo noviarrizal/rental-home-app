65f0803ffa89d50be392beb19211acdd59118f3c
noviarrizal <noviarrizalarif@gmail.com>
2022-03-28 22:07:17 +0800

    add fetchApi

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
 	const { data } = await axios.get(url, {
 		headers: {
 			"X-RapidAPI-Host": "bayut.p.rapidapi.com",
-			"X-RapidAPI-Key": process.env.RAPID_API_KEY,
+			"X-RapidAPI-Key": "bf3ff17fbcmsh1dbb4c0b6f597ffp112b71jsnf146bc3811b1",
 		},
 	});
 
